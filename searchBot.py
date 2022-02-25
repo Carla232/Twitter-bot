f929bf81dd183660bbba5618f9d407a1d82a67ff
Carla232 <79741526+Carla232@users.noreply.github.com>
2022-02-26 00:40:26 +0200

    Update searchBot.py

--- searchBot.py
+++ searchBot.py
@@ -1,11 +1,11 @@
 import tweepy
 import time
 
-consumer_key=''
-consumer_secret=''
+consumer_key='1017337919206514689-z9lypN5YIyh2hI6rdBSEDHXyw54gg3'
+consumer_secret='tc5lwZULyV1su0yDysoKZdq1b0CjYCIRubSXOli6CbBO1'
 
-key=''
-secret=''
+key='oDnoahwB5Q0W0oeDYxgtccqxn'
+secret='ROpGlh22E6wYB3igGsZpAqCjXESmc79tRLjqrORvr43rG8X57t'
 
 auth = tweepy.OauthHnadler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
